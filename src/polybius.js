37c4f12cb184006892cd9c87fafbfab91f2ba816
fulbrecht <daniel.fulbrecht@gmail.com>
2022-06-23 00:54:49 -0400

    started polybius()

--- src/polybius.js
+++ src/polybius.js
@@ -6,8 +6,66 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
+  function decodePair(col, row){
+    const grid = [
+      ["a","b","c","d","e"],
+      ["f","g","h","(i/j)","k"],
+      ["l","m","n","o","p"],
+      ["q","r","s","t","u"],
+      ["v","w","x","y","z"]
+    ]
+
+    return grid[row-1][col-1];
+  }
+
+  function encodeChar(ch){
+    if(ch === " "){
+      return " ";
+    };
+
+    if(ch === "i" || ch === "j"){
+      return "42"
+    }
+
+    const grid = [
+      ["a","b","c","d","e"],
+      ["f","g","h","(i/j)","k"],
+      ["l","m","n","o","p"],
+      ["q","r","s","t","u"],
+      ["v","w","x","y","z"]
+    ]
+    let coord = [];
+
+    for(const row in grid){
+      if( grid[row].includes(ch)){
+        coord.push(grid[row].indexOf(ch) + 1);
+        coord.push(parseInt(row) + 1);
+        //console.log(`row: ${row}, col: ${grid[row].indexOf(ch)}`);
+      }
+    }
+    return coord.join('');
+
+  }
+
   function polybius(input, encode = true) {
     // your solution code here
+    let result = [];
+
+    //encoding
+    if(encode){
+      for(const ch in input){
+        result.push(encodeChar(input[ch]));
+        // console.log(`ch: ${input[ch]}, result: ${result}`)
+      };
+    }
+    else {
+      for(const ch in input){
+        result.push(decodePair(input[ch]));
+      };
+    };
+
+
+    return result.join('')
   }
 
   return {
