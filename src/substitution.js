d12925e45b284c449af3e1b8274657c549dc9894
fulbrecht <daniel.fulbrecht@gmail.com>
2022-06-26 23:52:39 -0400

    completed substitution() function and uncommented all tests

--- src/substitution.js
+++ src/substitution.js
@@ -6,8 +6,50 @@
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
+  function isUnique(alphabet){
+    let check = alphabet;
+    for (const index in alphabet){
+      const char = alphabet[index];
+      if(alphabet.indexOf(char) !== alphabet.lastIndexOf(char)){
+        return false;
+      }
+    }
+    return true;
+  };
+
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    //check that alphabet exitst, the length of alphabet, and that no characters repeat. 
+    if( !alphabet || alphabet.length !== 26 || !isUnique(alphabet)){
+      return false;
+    }
+
+    //create cypher object with normal alphabet as keys and substitution alphabet as values. reverse if encode = false.
+    let alpha = "abcdefghijklmnopqrstuvwxyz"
+    let cypher = {}
+    
+    for( const index in alphabet){
+      if(encode){
+        cypher[alpha[index]] = alphabet[index];
+      } else {
+        cypher[alphabet[index]] = alpha[index];
+      }
+    };
+    
+    //for each letter in input, look up value based on key and add new letter to result
+    const keys = Object.keys(cypher).join("");
+    const inputLower = input.toLowerCase();
+    let result = "";
+
+    for(const index in inputLower){
+      const letter = inputLower[index];
+      if(keys.includes(letter)){
+        result += cypher[letter];
+      } else {
+        result += letter;
+      }
+    }
+
+    return result;
   }
 
   return {
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,48 +1,48 @@
 // Write your tests here!
-// const { expect } = require("chai");
-// const { caesar } = require("../src/caesar");
+const { expect } = require("chai");
+const { caesar } = require("../src/caesar");
 
-// describe("caesar() tests written by Daniel Fulbrecht", () => {
-//     it("should shift the input by a positive shift number", () => {
-//         const input = "thinkful"
-//         const actual = caesar(input, 3);
-//         const expected = "wklqnixo"
-//         expect(actual).to.equal(expected);
-//     });
+describe("caesar() tests written by Daniel Fulbrecht", () => {
+    it("should shift the input by a positive shift number", () => {
+        const input = "thinkful"
+        const actual = caesar(input, 3);
+        const expected = "wklqnixo"
+        expect(actual).to.equal(expected);
+    });
 
-//     it("should shift the input by a negative shift number", () => {
-//         const input = "thinkful"
-//         const actual = caesar(input, -3);
-//         const expected = "qefkhcri"
-//         expect(actual).to.equal(expected);
-//     });
+    it("should shift the input by a negative shift number", () => {
+        const input = "thinkful"
+        const actual = caesar(input, -3);
+        const expected = "qefkhcri"
+        expect(actual).to.equal(expected);
+    });
 
-//     it("should decode the input when encode set to false", () => {
-//         const input = "wklqnixo"
-//         const actual = caesar(input, 3, false);
-//         const expected = "thinkful"
-//         expect(actual).to.equal(expected);
-//     });
+    it("should decode the input when encode set to false", () => {
+        const input = "wklqnixo"
+        const actual = caesar(input, 3, false);
+        const expected = "thinkful"
+        expect(actual).to.equal(expected);
+    });
     
-//     it("should maintain spaces and nonalphabetic symbols", () => {
-//         const input = "This is a secret message!"
-//         const actual = caesar(input, 8);
-//         const expected = "bpqa qa i amkzmb umaaiom!"
-//         expect(actual).to.equal(expected);
-//     });
+    it("should maintain spaces and nonalphabetic symbols", () => {
+        const input = "This is a secret message!"
+        const actual = caesar(input, 8);
+        const expected = "bpqa qa i amkzmb umaaiom!"
+        expect(actual).to.equal(expected);
+    });
 
-//     it("should ignore capital letters", () => {
-//         const input = "BPQA qa I amkzmb umaaiom!"
-//         const actual = caesar(input, 8, false);
-//         const expected = "this is a secret message!"
-//         expect(actual).to.equal(expected);
-//     });
+    it("should ignore capital letters", () => {
+        const input = "BPQA qa I amkzmb umaaiom!"
+        const actual = caesar(input, 8, false);
+        const expected = "this is a secret message!"
+        expect(actual).to.equal(expected);
+    });
 
-//     it("should return false if shift is 0, >25 , <-25", () => {
-//         const input = "thinkful"
+    it("should return false if shift is 0, >25 , <-25", () => {
+        const input = "thinkful"
 
-//         expect(caesar(input)).to.be.false;
-//         expect(caesar(input, 99)).to.be.false;
-//         expect(caesar(input, -26)).to.be.false;
-//     });
-// });
+        expect(caesar(input)).to.be.false;
+        expect(caesar(input, 99)).to.be.false;
+        expect(caesar(input, -26)).to.be.false;
+    });
+});
